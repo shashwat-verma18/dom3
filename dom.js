02f446a2ae9b20184a97536595241c01d37dada2
Shashwat Verma <shashwatv18@gmail.com>
2023-12-04 14:50:59 +0530

    Modify Buttons

--- dom.js
+++ dom.js
@@ -7,17 +7,24 @@ form.addEventListener('submit', addItem);
 
 itemList.addEventListener('click', removeItem);
 
+filter.addEventListener('keyup', filterItems);
+
+editbtn(0);
+
 function addItem(e){
   e.preventDefault();
 
   var newItem = document.getElementById('item').value;
 
+  var newDes = document.getElementById('item2').value;
 
+  var val = newItem+' '+newDes;
+  
   var li = document.createElement('li');
 
   li.className = 'list-group-item';
 
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(val));
     
 
   var deleteBtn = document.createElement('button');
@@ -33,6 +40,13 @@ function addItem(e){
 
 
   itemList.appendChild(li);
+
+  var items = document.getElementsByClassName('list-group-item');
+
+  console.log('Length : ',items.length);
+  editbtn(items.length-1);
+
+
 }
 
 
@@ -45,26 +59,38 @@ function removeItem(e){
   }
 }
 
+function editbtn(x){
+    var items = document.getElementsByClassName('list-group-item');
 
-var editBtn = document.createElement('edit');
-editBtn.className = 'btn-sm float-right';
-editBtn.appendChild(document.createTextNode('E'));
-editBtn.style.background = 'green';
-editBtn.style.color = 'white';
-editBtn.style.paddingBottom = '5px';
-editBtn.style.marginRight = '5px';
+    for(var i=x;i<items.length;i++){
+        var editBtn = document.createElement('edit');
+        editBtn.className = 'btn-sm float-right';
+        editBtn.appendChild(document.createTextNode('Edit'));
+        editBtn.style.background = 'green';
+        editBtn.style.color = 'white';
+        editBtn.style.paddingBottom = '5px';
+        editBtn.style.marginRight = '5px';
 
-var items = document.getElementsByClassName('list-group-item');
-
-for(var i=0;i<items.length;i++){
-    var editBtn = document.createElement('edit');
-    editBtn.className = 'btn-sm float-right';
-    editBtn.appendChild(document.createTextNode('E'));
-    editBtn.style.background = 'green';
-    editBtn.style.color = 'white';
-    editBtn.style.paddingBottom = '5px';
-    editBtn.style.marginRight = '5px';
-
-    items[i].appendChild(editBtn);
+        items[i].appendChild(editBtn);
+    }
 }
+
+
+function filterItems(e){
+
+    var text = e.target.value.toLowerCase();
+
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+ }
+
+
 
